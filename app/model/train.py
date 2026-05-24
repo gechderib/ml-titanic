2a3433732e1c052d430a93cf2499dc5ff0c6f384
gechderib <gechderib@gmail.com>
2026-05-24 17:18:06 +0300

    adding the publish in rabitmq

--- app/model/train.py
+++ app/model/train.py
@@ -7,12 +7,11 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
+from sklearn.preprocessing import MinMaxScaler
 
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.preprocessing import MinMaxScaler
 
 load_dotenv()
 
